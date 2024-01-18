9d86234a46c378893eb63ca35469610b71fc73f0
Filip Tasevski <135996963+filipinec@users.noreply.github.com>
2024-01-18 23:00:31 +0100

    update

---
+++ script.js
@@ -0,0 +1,22 @@
+let num = document.querySelector("#number");
+let counter = 0;
+// setInterval(() => {
+//  if (counter == 65) {
+//   clearInterval();
+//  } else {
+//   counter = counter + 1;
+//   num.innerHTML = counter + "%";
+//  }
+// }, 30);
+
+const x = function (setNum) {
+ setInterval(() => {
+  if (counter == setNum) {
+   clearInterval();
+  } else {
+   counter = counter + 1;
+   num.innerHTML = counter + "%";
+  }
+ }, 30);
+};
+x(65);
