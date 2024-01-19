23889087db1912b1c4b863d9d41f2bcfa679ff3d
Filip Tasevski <135996963+filipinec@users.noreply.github.com>
2024-01-19 14:51:30 +0100

    update

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 let num = document.querySelector("#number");
 let counter = 0;
-// setInterval(() => {
-//  if (counter == 65) {
-//   clearInterval();
-//  } else {
-//   counter = counter + 1;
-//   num.innerHTML = counter + "%";
-//  }
-// }, 30);
 
-const x = function (setNum) {
- setInterval(() => {
-  if (counter == setNum) {
-   clearInterval();
-  } else {
-   counter = counter + 1;
-   num.innerHTML = counter + "%";
-   
-  }
- }, 30);
-};
-x(65);
+setInterval(() => {
+ if (counter == 65) {
+  clearInterval();
+ } else {
+  counter = counter + 1;
+  num.innerHTML = counter + "%";
+ }
+}, 30);
+
+// const x = function (setNum) {
+//  setInterval(() => {
+//   if (counter == setNum) {
+//    clearInterval();
+//   } else {
+//    counter = counter + 1;
+//    num.innerHTML = counter + "%";
+//    //  num.style.setProperty("--my-variable", 218);
+//   }
+//  }, 30);
+// };
+// x(65);
