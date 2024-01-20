bb54191a6023af116b000152341e873bf84bbb89
Filip Tasevski <135996963+filipinec@users.noreply.github.com>
2024-01-20 12:58:36 +0100

    update

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
-// Modify the function to accept the skillId parameter
 const progressBar = function (skillId, numOfCircle, num, timeForLoad) {
-  let numElement = document.querySelector(`#${skillId} .inner`);
-  let root = document.documentElement;
-  let counter = 0;
-  let intervalId;
- 
-  root.style.setProperty("--changeHTML", numOfCircle + 216);
-  root.style.setProperty("--changeCSS", numOfCircle + 106);
- 
-  intervalId = setInterval(() => {
-   if (counter == num) {
-    clearInterval(intervalId);
-   } else {
-    counter = counter + 1;
-    numElement.innerHTML = counter + "%";
-    console.log(numElement);
-   }
-  }, timeForLoad);
- };
- 
- // Call the function for each skill with unique parameters
- progressBar("skill-1", 0, 50, 40);
- progressBar("skill-2", 0, 75, 27);
- 
- // Add more calls for additional skills
- 
+ let numElement = document.querySelector(`#${skillId} .inner`);
+ let root = document.documentElement;
+ let counter = 0;
+ let intervalId;
+
+ root.style.setProperty("--changeHTML", numOfCircle + 216);
+ root.style.setProperty("--changeCSS", numOfCircle + 106);
+
+ intervalId = setInterval(() => {
+  if (counter == num) {
+   clearInterval(intervalId);
+  } else {
+   counter = counter + 1;
+   numElement.innerHTML = counter + "%";
+   console.log(numElement);
+  }
+ }, timeForLoad);
+};
+
+progressBar("skill-1", 0, 50, 40);
+progressBar("skill-2", 0, 75, 27);
